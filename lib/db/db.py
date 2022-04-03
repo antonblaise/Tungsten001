e00a2d54e78848243f972d9c1d3989f462e971c0
antonblaise <antoniusblaise@gmail.com>
2022-04-04 03:52:06 +0800

    Update db.py

--- lib/db/db.py
+++ lib/db/db.py
@@ -2,8 +2,8 @@ from os.path import isfile
 from sqlite3 import connect
 from apscheduler.triggers.cron import CronTrigger 
 
-DB_PATH = "./data/db/database.db"
-BUILD_PATH = "./data/db/build.sql"
+DB_PATH = open("./data/db/database.db", "a+")
+BUILD_PATH = open("./data/db/build.sql", "a+")
 
 cxn = connect(DB_PATH, check_same_thread=False)
 cur = cxn.cursor()
